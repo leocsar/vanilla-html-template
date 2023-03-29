48162cba6cc61d596a5bf06d01ac5f6a3f7c9101
Leo César <contato@leocesar.com.br>
2023-03-28 21:11:33 -0300

    Fix again

--- service-worker.js
+++ service-worker.js
@@ -2,8 +2,6 @@ self.addEventListener('fetch', function(event) {
   event.respondWith(async function() {
     try{
       var res = await fetch(event.request);
-      var cache = await caches.open('cache');
-      cache.put(event.request.url, res.clone());
       return res;
     }
     catch(error){
